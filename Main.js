f2f6c1de2b1f2f57a65fa030ae55e175a8f89c98
gabsillis <gianniabsillis@gmail.com>
2016-04-19 11:18:21 -0400

    Update Main.js
    Made Enum for diatonic chords

--- Main.js
+++ Main.js
@@ -19,7 +19,36 @@ function close() {
   //Runs when plugin is closed down
 };
  
- 
+// Chord Definitions
+my.namespace.DiatonicChords = {
+  I      : 1,
+  II     : 2,
+  III    : 3,
+  IV     : 4,
+  V      : 5,
+  VI     : 6,
+  VII    : 7,
+  i      : 8,
+  ii     : 9,
+  iii    : 10,
+  iv     : 11,
+  v      : 12,
+  vi     : 13,
+  vii    : 14,
+  Iaug   : 15,
+  IIaug  : 16,
+  IIIaug : 17,
+  IVaug  : 18,
+  Vaug   : 19,
+  VIaug  : 20,
+  VIIaug : 21,
+  idim   : 22,
+  iidim  : 23,
+  iiidim : 24,
+  ivdim  : 25,
+  vidim  : 26,
+  viidim : 27,
+}
  
  
  
